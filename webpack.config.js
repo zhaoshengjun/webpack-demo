749e755545131e64bb2303281c5e650c8b453099
zhaoshengjun <zhao.shengjun@gmail.com>
2016-07-13 10:53:13 +1000

    add webpack-merge and webpack-validator

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: {
     app: PATHS.app
   },
@@ -18,3 +20,15 @@ module.exports = {
     new HtmlWebpackPlugin({title:'Webpack Demo'})
   ]
 }
+
+var config;
+
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(common, {});
+}
+
+module.exports = validate(config);
